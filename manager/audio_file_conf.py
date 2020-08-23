cb87f23277f26f4d347c1e21a351f680ded6e0cf
teisha <ahsiet4@yahoo.com>
2020-08-23 10:52:55 -0500

    added audio processing

---
+++ manager/audio_file_conf.py
@@ -0,0 +1,174 @@
+keysToGet = ["System.Audio.ChannelCount",
+    "System.Audio.Compression",
+    "System.Audio.EncodingBitrate",
+    "System.Audio.Format",
+    "System.Audio.IsVariableBitRate",
+    "System.Audio.PeakValue",
+    "System.Audio.SampleRate",
+    "System.Audio.SampleSize",
+    "System.Audio.StreamName",
+    "System.Audio.StreamNumber",
+    "System.Media.AuthorUrl",
+    "System.Media.AverageLevel",
+    "System.Media.ClassPrimaryID",
+    "System.Media.ClassSecondaryID",
+    "System.Media.CollectionGroupID",
+    "System.Media.CollectionID",
+    "System.Media.ContentDistributor",
+    "System.Media.ContentID",
+    "System.Media.CreatorApplication",
+    "System.Media.CreatorApplicationVersion",
+    "System.Media.DateEncoded",
+    "System.Media.DateReleased",
+    "System.Media.Duration",
+    "System.Media.DVDID",
+    "System.Media.EncodedBy",
+    "System.Media.EncodingSettings",
+    "System.Media.FrameCount",
+    "System.Media.MCDI",
+    "System.Media.MetadataContentProvider",
+    "System.Media.Producer",
+    "System.Media.PromotionUrl",
+    "System.Media.ProtectionType",
+    "System.Media.ProviderRating",
+    "System.Media.ProviderStyle",
+    "System.Media.Publisher",
+    "System.Media.SubscriptionContentId",
+    "System.Media.SubTitle",
+    "System.Media.UniqueFileIdentifier",
+    "System.Media.UserNoAutoInfo",
+    "System.Media.UserWebUrl",
+    "System.Media.Writer",
+    "System.Media.Year",
+    "System.Music.AlbumArtist",
+    "System.Music.AlbumTitle",
+    "System.Music.Artist",
+    "System.Music.BeatsPerMinute",
+    "System.Music.Composer",
+    "System.Music.Conductor",
+    "System.Music.ContentGroupDescription",
+    "System.Music.Genre",
+    "System.Music.InitialKey",
+    "System.Music.Lyrics",
+    "System.Music.Mood",
+    "System.Music.PartOfSet",
+    "System.Music.Period",
+    "System.Music.SynchronizedLyrics",
+    "System.Music.TrackNumber",
+    "System.Author",
+    "System.Capacity",
+    "System.Category",
+    "System.Comment",
+    "System.Company",
+    "System.ComputerName",
+    "System.FileName",
+    "System.FileOwner"
+    ]    
+
+
+'''
+
+PKEY_AcquisitionID = (IID('{65A98875-3C80-40AB-ABBC-EFDAF77DBEE2}'), 100)
+PKEY_ApplicationName = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 18)
+PKEY_Author = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 4)
+PKEY_Capacity = (IID('{9B174B35-40FF-11D2-A27E-00C04FC30871}'), 3)
+PKEY_Category = (IID('{D5CDD502-2E9C-101B-9397-08002B2CF9AE}'), 2)
+PKEY_Comment = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 6)
+PKEY_Company = (IID('{D5CDD502-2E9C-101B-9397-08002B2CF9AE}'), 15)
+PKEY_ComputerName = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 5)
+PKEY_ContainedItems = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 29)
+PKEY_ContentStatus = (IID('{D5CDD502-2E9C-101B-9397-08002B2CF9AE}'), 27)
+PKEY_ContentType = (IID('{D5CDD502-2E9C-101B-9397-08002B2CF9AE}'), 26)
+PKEY_Copyright = (IID('{64440492-4C8B-11D1-8B70-080036B11A03}'), 11)
+PKEY_DateAccessed = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 16)
+PKEY_DateAcquired = (IID('{2CBAA8F5-D81F-47CA-B17A-F8D822300131}'), 100)
+PKEY_DateArchived = (IID('{43F8D7B7-A444-4F87-9383-52271C9B915C}'), 100)
+PKEY_DateCompleted = (IID('{72FAB781-ACDA-43E5-B155-B2434F85E678}'), 100)
+PKEY_DateCreated = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 15)
+PKEY_DateImported = (IID('{14B81DA1-0135-4D31-96D9-6CBFC9671A99}'), 18258)
+PKEY_DateModified = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 14)
+PKEY_DueDate = (IID('{3F8472B5-E0AF-4DB2-8071-C53FE76AE7CE}'), 100)
+PKEY_EndDate = (IID('{C75FAA05-96FD-49E7-9CB4-9F601082D553}'), 100)
+PKEY_FileAllocationSize = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 18)
+PKEY_FileAttributes = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 13)
+PKEY_FileCount = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 12)
+PKEY_FileDescription = (IID('{0CEF7D53-FA64-11D1-A203-0000F81FEDEE}'), 3)
+PKEY_FileExtension = (IID('{E4F10A3C-49E6-405D-8288-A23BD4EEAA6C}'), 100)
+PKEY_FileFRN = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 21)
+PKEY_FileName = (IID('{41CF5AE0-F75A-4806-BD87-59C7D9248EB9}'), 100)
+PKEY_FileOwner = (IID('{9B174B34-40FF-11D2-A27E-00C04FC30871}'), 4)
+PKEY_FileVersion = (IID('{0CEF7D53-FA64-11D1-A203-0000F81FEDEE}'), 4)
+PKEY_FindData = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 0)
+PKEY_FlagColor = (IID('{67DF94DE-0CA7-4D6F-B792-053A3E4F03CF}'), 100)
+PKEY_FlagColorText = (IID('{45EAE747-8E2A-40AE-8CBF-CA52ABA6152A}'), 100)
+PKEY_FlagStatus = (IID('{E3E0584C-B788-4A5A-BB20-7F5A44C9ACDD}'), 12)
+PKEY_FlagStatusText = (IID('{DC54FD2E-189D-4871-AA01-08C2F57A4ABC}'), 100)
+PKEY_FreeSpace = (IID('{9B174B35-40FF-11D2-A27E-00C04FC30871}'), 2)
+PKEY_Identity = (IID('{A26F4AFC-7346-4299-BE47-EB1AE613139F}'), 100)
+PKEY_Importance = (IID('{E3E0584C-B788-4A5A-BB20-7F5A44C9ACDD}'), 11)
+PKEY_ImportanceText = (IID('{A3B29791-7713-4E1D-BB40-17DB85F01831}'), 100)
+PKEY_IsAttachment = (IID('{F23F425C-71A1-4FA8-922F-678EA4A60408}'), 100)
+PKEY_IsDeleted = (IID('{5CDA5FC8-33EE-4FF3-9094-AE7BD8868C4D}'), 100)
+PKEY_IsFlagged = (IID('{5DA84765-E3FF-4278-86B0-A27967FBDD03}'), 100)
+PKEY_IsFlaggedComplete = (IID('{A6F360D2-55F9-48DE-B909-620E090A647C}'), 100)
+PKEY_IsIncomplete = (IID('{346C8BD1-2E6A-4C45-89A4-61B78E8E700F}'), 100)
+PKEY_IsRead = (IID('{E3E0584C-B788-4A5A-BB20-7F5A44C9ACDD}'), 10)
+PKEY_IsSendToTarget = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 33)
+PKEY_IsShared = (IID('{EF884C5B-2BFE-41BB-AAE5-76EEDF4F9902}'), 100)
+PKEY_ItemAuthors = (IID('{D0A04F0A-462A-48A4-BB2F-3706E88DBD7D}'), 100)
+PKEY_ItemDate = (IID('{F7DB74B4-4287-4103-AFBA-F1B13DCD75CF}'), 100)
+PKEY_ItemFolderNameDisplay = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 2)
+PKEY_ItemFolderPathDisplay = (IID('{E3E0584C-B788-4A5A-BB20-7F5A44C9ACDD}'), 6)
+PKEY_ItemFolderPathDisplayNarrow = (IID('{DABD30ED-0043-4789-A7F8-D013A4736622}'), 100)
+PKEY_ItemName = (IID('{6B8DA074-3B5C-43BC-886F-0A2CDCE00B6F}'), 100)
+PKEY_ItemNameDisplay = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 10)
+PKEY_ItemNamePrefix = (IID('{D7313FF1-A77A-401C-8C99-3DBDD68ADD36}'), 100)
+PKEY_ItemParticipants = (IID('{D4D0AA16-9948-41A4-AA85-D97FF9646993}'), 100)
+PKEY_ItemPathDisplay = (IID('{E3E0584C-B788-4A5A-BB20-7F5A44C9ACDD}'), 7)
+PKEY_ItemPathDisplayNarrow = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 8)
+PKEY_ItemType = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 11)
+PKEY_ItemTypeText = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 4)
+PKEY_ItemUrl = (IID('{49691C90-7E17-101A-A91C-08002B2ECDA9}'), 9)
+PKEY_Keywords = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 5)
+PKEY_Kind = (IID('{1E3EE840-BC2B-476C-8237-2ACD1A839B22}'), 3)
+PKEY_KindText = (IID('{F04BEF95-C585-4197-A2B7-DF46FDC9EE6D}'), 100)
+PKEY_Language = (IID('{D5CDD502-2E9C-101B-9397-08002B2CF9AE}'), 28)
+PKEY_MileageInformation = (IID('{FDF84370-031A-4ADD-9E91-0D775F1C6605}'), 100)
+PKEY_MIMEType = (IID('{0B63E350-9CCC-11D0-BCDB-00805FCCCE04}'), 5)
+PKEY_Null = (IID('{00000000-0000-0000-0000-000000000000}'), 0)
+PKEY_OfflineAvailability = (IID('{A94688B6-7D9F-4570-A648-E3DFC0AB2B3F}'), 100)
+PKEY_OfflineStatus = (IID('{6D24888F-4718-4BDA-AFED-EA0FB4386CD8}'), 100)
+PKEY_OriginalFileName = (IID('{0CEF7D53-FA64-11D1-A203-0000F81FEDEE}'), 6)
+PKEY_ParentalRating = (IID('{64440492-4C8B-11D1-8B70-080036B11A03}'), 21)
+PKEY_ParentalRatingReason = (IID('{10984E0A-F9F2-4321-B7EF-BAF195AF4319}'), 100)
+PKEY_ParentalRatingsOrganization = (IID('{A7FE0840-1344-46F0-8D37-52ED712A4BF9}'), 100)
+PKEY_ParsingBindContext = (IID('{DFB9A04D-362F-4CA3-B30B-0254B17B5B84}'), 100)
+PKEY_ParsingName = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 24)
+PKEY_ParsingPath = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 30)
+PKEY_PerceivedType = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 9)
+PKEY_PercentFull = (IID('{9B174B35-40FF-11D2-A27E-00C04FC30871}'), 5)
+PKEY_Priority = (IID('{9C1FCF74-2D97-41BA-B4AE-CB2E3661A6E4}'), 5)
+PKEY_PriorityText = (IID('{D98BE98B-B86B-4095-BF52-9D23B2E0A752}'), 100)
+PKEY_Project = (IID('{39A7F922-477C-48DE-8BC8-B28441E342E3}'), 100)
+PKEY_ProviderItemID = (IID('{F21D9941-81F0-471A-ADEE-4E74B49217ED}'), 100)
+PKEY_Rating = (IID('{64440492-4C8B-11D1-8B70-080036B11A03}'), 9)
+PKEY_RatingText = (IID('{90197CA7-FD8F-4E8C-9DA3-B57E1E609295}'), 100)
+PKEY_Sensitivity = (IID('{F8D3F6AC-4874-42CB-BE59-AB454B30716A}'), 100)
+PKEY_SensitivityText = (IID('{D0C7F054-3F72-4725-8527-129A577CB269}'), 100)
+PKEY_SFGAOFlags = (IID('{28636AA6-953D-11D2-B5D6-00C04FD918D0}'), 25)
+PKEY_SharedWith = (IID('{EF884C5B-2BFE-41BB-AAE5-76EEDF4F9902}'), 200)
+PKEY_ShareUserRating = (IID('{64440492-4C8B-11D1-8B70-080036B11A03}'), 12)
+PKEY_Shell_OmitFromView = (IID('{DE35258C-C695-4CBC-B982-38B0AD24CED0}'), 2)
+PKEY_SimpleRating = (IID('{A09F084E-AD41-489F-8076-AA5BE3082BCA}'), 100)
+PKEY_Size = (IID('{B725F130-47EF-101A-A5F1-02608C9EEBAC}'), 12)
+PKEY_SoftwareUsed = (IID('{14B81DA1-0135-4D31-96D9-6CBFC9671A99}'), 305)
+PKEY_SourceItem = (IID('{668CDFA5-7A1B-4323-AE4B-E527393A1D81}'), 100)
+PKEY_StartDate = (IID('{48FD6EC8-8A12-4CDF-A03E-4EC5A511EDDE}'), 100)
+PKEY_Status = (IID('{000214A1-0000-0000-C000-000000000046}'), 9)
+PKEY_Subject = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 3)
+PKEY_Thumbnail = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 17)
+PKEY_ThumbnailCacheId = (IID('{446D16B1-8DAD-4870-A748-402EA43D788C}'), 100)
+PKEY_ThumbnailStream = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 27)
+PKEY_Title = (IID('{F29F85E0-4FF9-1068-AB91-08002B27B3D9}'), 2)
+'''
+
--- manager/audiobooks.py
+++ manager/audiobooks.py
@@ -1,8 +1,12 @@
 import json
 import os
+import shutil
+import math
+import datetime
 import mutagen
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2
+from mutagen.easyid3 import EasyID3
 
 
 import sys
@@ -11,6 +15,7 @@ import importlib
 from importlib import machinery 
 loader = importlib.machinery.SourceFileLoader('config', 'config/conf.py')
 config = loader.load_module('config')
+from audio_file_conf import keysToGet 
 
 import pythoncom
 from win32com.propsys import propsys, pscon
@@ -33,41 +38,147 @@ class AudioBookManager:
 
     def process_book(self, book_name: str):
         book = next((item for item in self.bookList if item["title"] == book_name), dict())
-        print("AMAZON METADATA: ")
-        print(book)
+        print("PROCESSING: ")
+        # print(book)
+        # print(book["genre"])
+        print(book["author"])
+        print(book["title"])
+        # print(book["narrated_by"])
+        # print(book["filename"])
         filename = "{}{}.mp3".format(config.origin_path,book["filename"])
+        if not os.path.isfile(filename):
+            print("File does not exist", filename)
+            return
+
         print("AUDIO INFO: ")
         audio = MP3(filename)
+
         print(audio.info.__dict__)
         print(audio.info.bitrate)
-        audio_id3 = ID3(filename, translate=False)
+        # audio_id3 = ID3(filename, translate=False)
+        # print(audio_id3.keys())
         print("ID3 TAG INFO: ")
-        print (audio_id3.keys())
+        # print(EasyID3.valid_keys.keys())
+        audio_id3 = EasyID3(filename)
+        release_date: datetime = datetime.datetime.strptime(book["release_date"], '%d-%b-%Y')
+        audio_id3["genre"] = [u"Audiobook", book["genre"] ]
+        audio_id3["performer"] = book["narrated_by"]
+        audio_id3["author"] = book["author"]
+        # audio_id3["date"] = str(release_date.year)
+        audio_id3['copyright'] = book["copyright"]
+        audio_id3['language'] = book["language"]
+        audio_id3['asin'] = book["asin"]
+        audio.pprint()
+        # audio_id3['length'] = math.floor(audio.info.length )
+        audio_id3.save()
+        for keyname in EasyID3.valid_keys.keys():
+            try:
+                print (keyname , ":   ", audio_id3[keyname]   )
+            except:
+                pass
+                   
+        # print(audio_id3["genre"])
+        # print(audio_id3["artist"])
+        # print(audio_id3["albumartist"])
+        # print(audio_id3["album"])
+        # print(audio_id3["title"])
+        # try:
+        #     print(audio_id3["performer"])
+        # except:
+        #     print('Performer not found')
+        # audio_id3["artist"] = book["author"]
+        # audio_id3["albumartist"] = book["author"]
+        # audio_id3["album"] = book["title"]
+        # audio_id3["title"] = book["title"]
+
+# artist
+# albumartist
+# album
+# title
+# performer    
+        # audio_id3.save()
+        print("FILE TAGS:")
         for frame in mutagen.File(filename).tags.getall("TXXX"):
-            print(frame.__dict__)
-            print("{}".format(frame.desc ) )
+            print("{} : {}".format(frame.desc , frame.text if frame.desc != 'json64' else "json stuff" ) )
         print('TAGS:')
-        tags=mutagen.File(filename).tags
+        tag_file = mutagen.File(filename)
+        tags = tag_file.tags
         print(tags.keys())
+        for tag in tags.keys():
+            if tag.startswith('T'):
+                try:
+                    print(tag, ":   ", tag_file.tags[tag] )
+                except:
+                    pass
         print("OS FILE PROPERTIES: ")
         os_file = os.stat(filename)
         print(os_file)
 
+        self.move_file_to_desination("{}.mp3".format(book["filename"]), book["author"], book["title"])
+# System.Audio.ChannelCount:  2
+# System.Audio.Compression:  None
+# System.Audio.EncodingBitrate:  55584
+# System.Audio.Format:  {00000055-0000-0010-8000-00AA00389B71}
+# System.Audio.IsVariableBitRate:  True
+# System.Audio.PeakValue:  None
+# System.Audio.SampleRate:  22050
+# System.Audio.SampleSize:  16        
+# System.Media.Duration:  584256522448        
+# System.Media.EncodingSettings:  Lavf58.35.102
+# System.Media.Year:  2009        
+# System.Music.AlbumArtist:  Bernard Cornwell
+# System.Music.AlbumTitle:  Agincourt (Unabridged)
+# System.Music.Artist:  ['Bernard Cornwell'] 
+# System.Music.Genre:  ['Audiobook']     
+# System.Author:  ['Bernard Cornwell']
+# System.ComputerName:  DESKTOP-KI6MHU3
+# System.FileName:  Agincourt.mp3
+# System.FileOwner:  DESKTOP-KI6MHU3\laima  
+
+    def get_windows_sys_props(self, filename):
         print("WIN32COM.PROPSYS FILE PROPERTIES: ")
         pk = propsys.PSGetPropertyKeyFromName("System.Keywords")
         # get property store for a given shell item (here a file)
-        print(pk)
         #  MAKE SURE YOU USE THE RIGHT SLASHES HERE:
         ps = propsys.SHGetPropertyStoreFromParsingName(filename.replace('/','\\'))
+#  build an array of string type PROPVARIANT
+# newValue = propsys.PROPVARIANTType(["hello", "world"], pythoncom.VT_VECTOR | pythoncom.VT_BSTR)
+
+# # write property
+# ps.SetValue(pk, newValue)
+# ps.Commit()
         # read & print existing (or not) property value, System.Keywords type is an array of string
         title = ps.GetValue(pscon.PKEY_Title).GetValue()
-        print (title, pscon)
+        print (title)
+        print("System.Audio.Format", ps.GetValue(propsys.PSGetPropertyKeyFromName("System.Audio.Format")).GetValue())
         keywords = ps.GetValue(pk).GetValue()
         print(keywords)
+        for keyname in keysToGet:
+            print("{}: ".format(keyname), ps.GetValue(propsys.PSGetPropertyKeyFromName(keyname)).GetValue())
+        
 
+    def move_file_to_desination(self, filename: str, author: str, title: str):
+        orig = config.origin_path + filename
+        dest_path = "{}{}/{}/".format(config.dest_path,author, title  , filename )
+        dest_file = "{}{}".format(dest_path  , filename )
+        print('Copy file to ', dest_file)
+        # Move a file from the directory d1 to d2
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+        shutil.move(orig, dest_file)
 
 
 if __name__ == "__main__":
     audioManager = AudioBookManager()
     audioManager.load_books()
     audioManager.process_book('Agincourt')
+    # audioManager.process_book('Altered Carbon')
+
+
+
+
+
+
+
+
+
+
