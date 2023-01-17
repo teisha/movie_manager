7c897976528385a1fdfcfac4036cb82562f01438
Teisha Buford <ahsiet4@yahoo.com>
2023-01-16 23:03:54 -0600

    get rid of windows

--- run_copy.bat
+++
@@ -1,2 +0,0 @@
-REM C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\copy_script.py
-C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\runner\process_downloads.py
---
+++ run_copy.sh
@@ -0,0 +1,3 @@
+# REM C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\copy_script.py
+# C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\runner\process_downloads.py
+venv/bin/python runner/process_downloads.py
