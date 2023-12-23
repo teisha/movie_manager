87141896551c5d307066b00afe16ac005d79939a
Teisha Buford <ahsiet4@yahoo.com>
2023-12-23 14:38:28 -0600

    Add cmd to remount drive in WSL

--- run_copy.sh
+++ run_copy.sh
@@ -1,3 +1,4 @@
 # REM C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\copy_script.py
 # C:\git\movie_manager\plex_env\Scripts\python.exe C:\git\movie_manager\runner\process_downloads.py
+# sudo mount -t drvfs '\\nas\video' /mnt/m
 venv/bin/python runner/process_downloads.py
